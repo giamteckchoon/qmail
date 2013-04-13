04c7f908b1e40a3e374955a86fa837a04346a88e
Giam Teck Choon <giamteckchoon@gmail.com>
2013-04-14 04:29:16 +0800

    Increase memory buffer from 256 to 8k
    Read at http://www.mail-archive.com/qmail@id.wustl.edu/msg20935.html
    This patch increases the size of the memory buffer that qmail uses when
    querying the system for a list of all local IP addresses. This mailing list
    message describes the problem and includes the patch file.
    
    Signed-off-by: Giam Teck Choon <giamteckchoon@gmail.com>

--- ipme.c
+++ ipme.c
@@ -53,7 +53,7 @@ int ipme_init()
   if (!ipalloc_append(&ipme,&ix)) { return 0; }
   if ((s = socket(AF_INET,SOCK_STREAM,0)) == -1) return -1;
  
-  len = 256;
+  len = 8192; /* any value big enough to get all the interfaces in one read is good */
   for (;;) {
     if (!stralloc_ready(&buf,len)) { close(s); return 0; }
     buf.len = 0;
@@ -65,7 +65,7 @@ int ipme_init()
         break;
       }
     if (len > 200000) { close(s); return -1; }
-    len += 100 + (len >> 2);
+    len *= 2;
   }
   x = buf.s;
   while (x < buf.s + buf.len) {
